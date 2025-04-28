5deb17fbe75debb6ad13321b898790aff2140c9d
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-04-28 13:39:52 +0300

    feat(fs): rename wrongFilename.txt to properFilename.md

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const folderPath = path.join(__dirname, 'files');
+    const oldFilePath = path.join(folderPath, 'wrongFilename.txt');
+    const newFilePath = path.join(folderPath, 'properFilename.md');
+
+    try {
+        await fs.stat(oldFilePath);
+
+        try {
+            await fs.stat(newFilePath);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw err;
+            }
+        }
+
+        await fs.rename(oldFilePath, newFilePath);
+        console.log(`File renamed from ${oldFilePath} to ${newFilePath}`);
+    }catch (err) {
+        console.error('Error renaming file:', err.message);
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
