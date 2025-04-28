6b4c676a9d22d9053a5cf60cabd50a5429e6c9d6
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-04-28 22:28:15 +0300

    feat(streams): write to file using writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import {fileURLToPath} from "url";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const writeable = fs.createWriteStream(filePath);
+
+    process.stdin.on('data', (chunk) => {
+        writeable.write(chunk.toString());
+    })
+
+    process.stdin.on('end', () => {
+        writeable.end();
+    });
 };
 
 await write();
